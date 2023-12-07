05f5b67187a0047a8f90933588878e5cdeef8f6e
TabindaNoor <tabindanoor415@gmail.com>
2023-12-07 23:17:01 +0500

    not addin g

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -12,26 +12,48 @@ const Blog = () => {
     return () => clearInterval(timer);
   }, []);
 
+
+// https://brighttechvisions.com/why-should-robots-go-to-space-instead-of-humans/
+
+
+// https://brighttechvisions.com/nfs-network-file-system/
+
+// https://brighttechvisions.com/how-to-become-a-cloud-engineer/
+
+// https://brighttechvisions.com/see-the-future-now-with-ray-ban-meta-smart-glasses/
+
+// https://brighttechvisions.com/cybersecurity-experts/
+
+
+// https://brighttechvisions.com/can-robots-have-emotions-or-consciousness/
+
+
+// https://brighttechvisions.com/ai-assistant-vs-human-assistant/
+
+
+// https://brighttechvisions.com/why-gadgets-are-essential-in-the-digital-age/ 
+
+
    const blogs =[
     {
         img:"https://technofeat.net/wp-content/uploads/2023/09/T-2-1.webp",
         link:"https://technofeat.net/ray-ban-meta-smart-glasses-a-glimpse-into-the-future-of-technology/",
         date:"11 Dec, 2022",
-        title:"Top tools for Photographers",
+        title:"  5 Reasons Why Should Robots go to Space Instead of Humans",
         description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas."
     },
     {
         img:"https://kaleidoscopic-pie-2341a3.netlify.app/template/img/post-2.jpg",
         link:"https://technofeat.net/ray-ban-meta-smart-glasses-a-glimpse-into-the-future-of-technology/",
         date:"11 Dec, 2022",
-        title:"Top tools for Photographers",
+        title:"NFS (Network File System): Simplifying File Sharing Across Networks",
         description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas."
     },
     {
         img:"https://kaleidoscopic-pie-2341a3.netlify.app/template/img/post-3.jpg",
         link:"https://technofeat.net/ray-ban-meta-smart-glasses-a-glimpse-into-the-future-of-technology/",
         date:"11 Dec, 2023",
-        title:"Top tools for Photographers",
+        title:"Cloud Engineer        ",
         description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas."
     }
     ,
@@ -39,23 +61,36 @@ const Blog = () => {
         img:"https://kaleidoscopic-pie-2341a3.netlify.app/template/img/post-4.jpg",
         link:"https://technofeat.net/ray-ban-meta-smart-glasses-a-glimpse-into-the-future-of-technology/",
         date:"11 Dec, 2023",
-        title:"Top tools for Photographers",
+        title:"See the Future Now with Ray-Ban Meta Smart Glasses        ",
         description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas."
     },
     {
         img:"https://kaleidoscopic-pie-2341a3.netlify.app/template/img/post-5.jpg",
         link:"https://technofeat.net/ray-ban-meta-smart-glasses-a-glimpse-into-the-future-of-technology/",
         date:"11 Dec, 2023",
-        title:"Top tools for Photographers",
+        title:"10 Surprising Facts about CyberSecurity Experts         ",
         description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas."
     },
     {
         img:"https://kaleidoscopic-pie-2341a3.netlify.app/template/img/post-6.jpg",
         link:"https://technofeat.net/ray-ban-meta-smart-glasses-a-glimpse-into-the-future-of-technology/",
         date:"11 Dec, 2023",
-        title:"Top tools for Photographers",
+        title:"Can robots have emotions or consciousness?",
         description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas."
-    }
+    }, {
+        img:"https://kaleidoscopic-pie-2341a3.netlify.app/template/img/post-6.jpg",
+        link:"https://technofeat.net/ray-ban-meta-smart-glasses-a-glimpse-into-the-future-of-technology/",
+        date:"11 Dec, 2023",
+        title:"AI Assistant vs. Human Assistant: Which One is Better?",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas."
+    },
+    {
+        img:"https://kaleidoscopic-pie-2341a3.netlify.app/template/img/post-6.jpg",
+        link:"https://technofeat.net/ray-ban-meta-smart-glasses-a-glimpse-into-the-future-of-technology/",
+        date:"11 Dec, 2023",
+        title:"Why gadgets are essential in the digital age",
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas."
+    },
    ]
 
    const MyBlogCards =()=> {
@@ -120,6 +155,30 @@ const Blog = () => {
 
 <div className='grid  md:grid-cols-2 lg:grid-cols-3 mx-8 lg:mx-24 xl:mx-36 gap-x-4 gap-y-20   '>
     <MyBlogCards/>
+</div>
+
+<br />
+<div className='grid  md:grid-cols-2 lg:grid-cols-3 mx-8 lg:mx-24 xl:mx-36 gap-x-4 gap-y-20'>
+<div className="max-w-sm mx-auto overflow-hidden shadow-md">
+          <div className="relative">
+               <img
+              className="w-full"
+              src="https://technofeat.net/wp-content/uploads/2023/09/T-2-1.webp"
+              alt="BlogImage"
+            /> 
+            <a href='https://technofeat.net/ray-ban-meta-smart-glasses-a-glimpse-into-the-future-of-technology/' className="absolute bottom-0 left-0 p-2 text-white bg-green-500 rounded-md">11 Dec, 2022</a>
+                
+              </div>
+              
+
+            
+               <div className='text-white' >
+{/* {blog.link} */}
+                  <p className='text-2xl font-bold'> 5 Reasons Why Should Robots go to Space Instead of Humans</p>
+                  <p>Why Should Robots go to Space Instead of Humans? Robots can be a very good fit to be sent in space instead of humans. In the modern era of artificial intelligence and robots, machines have made life more easy and efficient. Humans are always interested in finding and exploring the depths of space.</p>
+              </div>
+            
+          </div>
 </div>
 <br /><br /><br /><br />
 
